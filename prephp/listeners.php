94be388fd7f7fcaa1440f155c60674b12f6d5379
nikic <privat@ni-po.com>
2010-03-07 21:10:21 +0100

    added require listener (preview)

--- prephp/listeners.php
+++ prephp/listeners.php
@@ -3,6 +3,7 @@
 	
 	include_once "listeners/coreListeners.php";
 	$core->registerTokenCompileListener(Prephp_Token::T_LINE, 'prephp_lineHardcoder');
+	$core->registerTokenListener(Prephp_Token::T_REQUIRE, 'prephp_require_listener');
 	
 	include_once "listeners/lambda.php";
 	$core->registerTokenListener(Prephp_Token::T_FUNCTION, 'prephp_lambda');
--- prephp/listeners/coreListeners.php
+++ prephp/listeners/coreListeners.php
@@ -2,4 +2,38 @@
 	function prephp_lineHardcoder($token) {
 		return (string)$token->getLine();
 	}
+	
+	function prephp_require_listener($tokenStream, $i) {
+		$tokenStream[$i] = new Prephp_Token(
+			Prephp_Token::T_STRING,
+			'prephp_require',
+			$tokenStream[$i]->getLine()
+		);
+		
+		if($tokenStream[$i+1]->is(Prephp_Token::T_OPEN_ROUND)) // require(), so its like a function
+			return;
+			
+		// otherwise insert ( and )
+		$i = $tokenStream->skipWhiteSpace($i);
+		$tokenStream->insertStreamAt($i,
+			array(
+				new Prephp_Token(
+					Prephp_Token::T_OPEN_ROUND,
+					'(',
+					$tokenStream[$i]->getLine()
+				)
+			)
+		);
+		
+		$i = $tokenStream->findNextToken($i, Prephp_Token::T_SEMICOLON);
+		$tokenStream->insertStreamAt($i,
+			array(
+				new Prephp_Token(
+					Prephp_Token::T_CLOSE_ROUND,
+					')',
+					$tokenStream[$i]->getLine()
+				)
+			)
+		);
+	}
 ?>

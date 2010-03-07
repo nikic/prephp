1a64f5f841ab15ca72dcd59bbc32e4d5268aac15
nikic <privat@ni-po.com>
2010-03-07 20:20:37 +0100

    made Core the core :D

--- prephp/Core.php
+++ prephp/Core.php
@@ -22,15 +22,58 @@
 	*/
 	
 	require_once 'TokenStream.php';
+	require_once 'Cache.php';
 	
 	class Prephp_Core
 	{
-		protected $tokenStream;
+		private static $instance; // Singleton
+		public static function get() {
+			if (!isset(self::$instance)) {
+				$c = __CLASS__;
+				self::$instance = new $c;
+			}
+			
+			return self::$instance;
+		}
+		final private function __construct() {}
+		final private function __clone() {}
+		
+		protected $cache;
+		
 		protected $tokenListeners;
 		protected $tokenCompileListeners;
 		
-		public function __construct($source) {
-			$this->tokenStream = new Prephp_Token_Stream($source);
+		public function createCache($sourceDir, $cacheDir) {
+			if(!is_writable($cacheDir)) {
+				throw new InvalidArgumentException("Cache Directory isn't wirteable");
+			}
+			
+			if(!is_readable($sourceDir)) {
+				throw new InvalidArgumentException("Source Directory isn't readable");
+			}
+			
+			$this->cache = new Prephp_Cache(
+				$sourceDir,
+				$cacheDir,
+				array(
+					$this,
+					'buildCallback'
+				)
+			);
+		}
+		
+		public function buildCallback($source, $cache) {
+			$sourceCont = file_get_contents($source);
+			if(!$sourceCont) {
+				throw new Exception('Could not read source file');
+			}
+			if(!file_put_contents($cache, $this->compile($sourceCont))) {
+				throw new Exception('Could not write cache file');
+			}
+		}
+		
+		public function buildFile($filename) {
+			return $this->cache->get($filename);
 		}
 		
 		public function addTokenListener($tokId, $callback) {
@@ -49,38 +92,37 @@
 			$this->tokenCompileListeners[$tokId][] = $callback;
 		}
 		
-		// walks tree and calls tokenListeners
-		protected function walkStream() {
-			foreach ($this->tokenStream as $i=>$token) {
+		public function compile($source) {
+			$tokenStream = new Prephp_Token_Stream(token_get_all($source));
+			
+			// first we walk the stream and call TokenListeners
+			foreach ($tokenStream as $i=>$token) {
 				if (isset($this->tokenListeners[$token->getTokenId()])) {
 					foreach ($this->tokenListeners[$token->getTokenId()] as $listener) {
-						call_user_func($listener, $this->tokenStream, $i);
+						call_user_func($listener, $tokenStream, $i);
 					}
 				}
 			}
-		}
-		
-		public function compile() {
-			$this->walkStream();
 			
+			// now we compile the source
 			$source = '';
-			
-			$numof = sizeof($this->tokenStream);
+			$numof = count($tokenStream);
 			for ($i = 0; $i < $numof; ++$i) {
-				if (isset($this->tokenCompileListeners[$this->tokenStream[$i]->getTokenId()])) {
-					foreach ($this->tokenCompileListeners[$this->tokenStream[$i]->getTokenId()] as $listener) {
-						$ret = call_user_func($listener, $this->tokenStream[$i]);
+				// in compiling we call TokenCompileListeners
+				if (isset($this->tokenCompileListeners[$tokenStream[$i]->getTokenId()])) {
+					foreach ($this->tokenCompileListeners[$tokenStream[$i]->getTokenId()] as $listener) {
+						$ret = call_user_func($listener, $tokenStream[$i]);
 						if ($ret != '') {
-							$this->tokenStream[$i] = new Prephp_Token(
-								$this->tokenStream[$i]->getTokenId(),
+							$tokenStream[$i] = new Prephp_Token(
+								$tokenStream[$i]->getTokenId(),
 								$ret,
-								$this->tokenStream[$i]->getLine()
+								$tokenStream[$i]->getLine()
 							);
 						}
 					}
 				}
 				
-				$source .= $this->tokenStream[$i]->getContent();
+				$source .= $tokenStream[$i]->getContent();
 			}
 			
 			return $source;

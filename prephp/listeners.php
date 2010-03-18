c1b67e96e25cbe013373abd12117585538b9dd83
nikic <privat@ni-po.com>
2010-03-18 18:56:14 +0100

    added version check to listeners

--- prephp/listeners.php
+++ prephp/listeners.php
@@ -7,14 +7,17 @@
 	$core->registerTokenCompileListener(T_FILE, 'prephp_FILE');
 	$core->registerTokenCompileListener(T_STRING, 'prephp_real_FILE');
 	
-	$core->registerTokenListener(T_STRING, 'prephp_DIR_simulator');
+	if (version_compare(PHP_VERSION, '5.3', '<')) {
+		$core->registerTokenListener(T_STRING, 'prephp_DIR_simulator');
+	}
 	$core->registerTokenListener(T_DIR, 'prephp_DIR');
 	
 	$core->registerTokenListener(array(T_REQUIRE, T_INCLUDE, T_REQUIRE_ONCE, T_INCLUDE_ONCE), 'prephp_include');
 	
-	
-	include_once "listeners/lambda.php";
-	$core->registerTokenListener(T_FUNCTION, 'prephp_lambda');
+	if (version_compare(PHP_VERSION, '5.3', '<')) {
+		include_once "listeners/lambda.php";
+		$core->registerTokenListener(T_FUNCTION, 'prephp_lambda');
+	}
 	
 	include_once "listeners/arrayAccess.php";
 	$core->registerTokenListener(array(T_STRING, T_VARIABLE), 'prephp_arrayAccess');

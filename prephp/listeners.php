31582b2d00bcb32dadac61e8ee410245a9c25de8
nikic <privat@ni-po.com>
2010-03-18 18:12:39 +0100

    removed Prephp_Token:: in listeners, added arrayAccess listener

--- prephp/listeners.php
+++ prephp/listeners.php
@@ -2,17 +2,20 @@
 	// $core is Prephp_Core
 	
 	include_once "listeners/coreListeners.php";
-	$core->registerTokenCompileListener(Prephp_Token::T_LINE, 'prephp_LINE');
+	$core->registerTokenCompileListener(T_LINE, 'prephp_LINE');
 	
-	$core->registerTokenCompileListener(Prephp_Token::T_FILE, 'prephp_FILE');
-	$core->registerTokenCompileListener(Prephp_Token::T_STRING, 'prephp_real_FILE');
+	$core->registerTokenCompileListener(T_FILE, 'prephp_FILE');
+	$core->registerTokenCompileListener(T_STRING, 'prephp_real_FILE');
 	
-	$core->registerTokenListener(Prephp_Token::T_STRING, 'prephp_DIR_simulator');
-	$core->registerTokenListener(Prephp_Token::T_DIR, 'prephp_DIR');
+	$core->registerTokenListener(T_STRING, 'prephp_DIR_simulator');
+	$core->registerTokenListener(T_DIR, 'prephp_DIR');
 	
-	$core->registerTokenListener(Prephp_Token::T_REQUIRE, 'prephp_include');
+	$core->registerTokenListener(array(T_REQUIRE, T_INCLUDE, T_REQUIRE_ONCE, T_INCLUDE_ONCE), 'prephp_include');
 	
 	
 	include_once "listeners/lambda.php";
-	$core->registerTokenListener(Prephp_Token::T_FUNCTION, 'prephp_lambda');
+	$core->registerTokenListener(T_FUNCTION, 'prephp_lambda');
+	
+	include_once "listeners/arrayAccess.php";
+	$core->registerTokenListener(array(T_STRING, T_VARIABLE), 'prephp_arrayAccess');
 ?>

7a81d2ddb2308babac091b8b451c0f669c373aa0
nikic <privat@ni-po.com>
2010-06-22 14:58:37 +0200

    minor changes in Path (nit)

--- prephp/classes/Path.php
+++ prephp/classes/Path.php
@@ -16,8 +16,11 @@
 			self::$win = PHP_OS == 'WINNT' || PHP_OS == 'WIN32';
 		}
 		
-		// expects: no restrictions
 		// returns all possible paths, where PHP will look whilst including
+		// filename: filename to resolve
+		// caller: the file calling the function
+		// executer: the entry file (the file called originally)
+		// include_path: include_path or if not specified get_include_path()
 		public static function possiblePaths($filename, $caller, $executer, $include_path = null) {
 			$executer = dirname($executer);
 			$caller = dirname($caller);
@@ -25,7 +28,6 @@
 				$include_path = get_include_path();
 			}
 			
-			// stream wrapper
 			if (self::isStreamWrapper($filename)) {
 				return array($filename);
 			}
@@ -43,7 +45,7 @@
 				
 				foreach ($include_paths as $path) {
 					try {
-						$paths[] = self::normalize($path.self::$slash.$filename, $executer);
+						$paths[] = self::normalize($path . self::$slash . $filename, $executer);
 					}
 					catch(InvalidArgumentException $e) {}
 				}
@@ -54,17 +56,20 @@
 			return $paths;
 		}
 		
-		// expects: no restrictions
+		// normalizes a path to an absolute one (no link resolution!)
+		// filename: the path/filename
+		// cwd: current working directory used to resolve relative paths
 		public static function normalize($filename, $cwd = '') {
-			// first check for stream wrapper
+			// check for stream wrapper
 			if (self::isStreamWrapper($filename)) {
-				return $filename; // no further processing (should one do something with file://?)
+				return $filename;
 			}
 			
 			// all further functions require normalized slashes;
 			$filename = self::normalizeSlashes($filename);
 			$cwd = self::normalizeSlashes($cwd);
 			
+			// if relative prepend cwd
 			if (!self::isAbsolute($filename)) {
 				if (!self::isAbsolute($cwd) && !self::isStreamWrapper($cwd)) {
 					throw new InvalidArgumentException('cwd is not absolute, not a stream wrapper or not specified!');
@@ -73,49 +78,42 @@
 				$filename = $cwd . self::$slash . $filename;
 			}
 			
-			// now resolve ./ and ../
-			$filename = array_reduce(explode(self::$slash, $filename), array(__CLASS__, 'normalizor'), 0);
+			$parts = explode(self::$slash, $filename);
+			$count = count($parts);
+			
+			$filename = array_shift($parts);
+			while ($part = array_shift($parts)) {
+				if ($part == '.') {
+					continue;
+				}
+				
+				if ($part == '..') {
+					$filename = dirname($filename);
+				}
+				else {
+					$filename .= self::$slash . $part;
+				}
+			}
 			
 			// strip multiple slashes
-			$filename = self::stripDoubleSlashes($filename);
-			
-			return $filename;
-		}
-		
-		// internal!
-		// used for array_reduce in self::normalize()
-		protected static function normalizor($current, $next) {
-			if ($next == '.') {
-				return $current;
-			}
-			
-			if ($next == '..') {
-				return dirname($current);
-			}
-			
-			if ($current === 0) { // initial
-				return $next;
-			}
-			return $current . self::$slash . $next;
+			return self::stripDoubleSlashes($filename);
 		}
 		
 		// normalizes slashes to self::$slash
-		// expects: non-wrapper filename
+		// filename: must be non-wrapper
 		public static function normalizeSlashes($filename) {
 			return str_replace(self::$antiSlash, self::$slash, $filename);
 		}
 		
-		// removes double slashes (///)
-		// expects: slash-normalized non-wrapper filename
+		// removes double slashes
+		// filename: must be slash-normalized non-wrapper
 		public static function stripDoubleSlashes($filename) {
 			return (self::isUNC($filename)?'\\':'') // do not strip \\ at beginning (UNC path)
 					. preg_replace('#'.preg_quote(self::$slash).'{2,}#', self::$slash, $filename);
 		}
 		
-		// chechs if is relative path
-		// ./ and ../
-		// (X:./ would be relative too, actually, but this is PHP's implementation)
-		// expects: slash-normalized filename
+		// checks if is relative path: (./ and ../)
+		// filename: must be slash-normalized
 		public static function isRelative($filename) {
 			$l = strlen($filename);
 			
@@ -125,7 +123,7 @@
 		}
 		
 		// checks if is absolute path (X: and UNC for win, / otherwise)
-		// expects: slash-normalized filename
+		// filename: must be slash-normalized
 		public static function isAbsolute($filename) {
 			$l = strlen($filename);
 			
@@ -134,7 +132,7 @@
 				return $l > 1 // minimum of two chars
 				&& (
 					(self::isSlash($filename[0]) && self::isSlash($filename[1])) // UNC path
-					|| (ctype_alpha($filename[0]) && $filename[1]==':') // X: path (actually, this may be relative, but PHP implements it this way)
+					|| (ctype_alpha($filename[0]) && $filename[1] == ':') // X: path
 				);
 			}
 			
@@ -143,28 +141,21 @@
 		}
 		
 		// check if is UNC-path (win only)
-		// expects: slash-normalized filename
+		// filename: must be slash-normalized
 		public static function isUNC($filename) {
 			return self::$win && strlen($filename) > 1 && self::isSlash($filename[0]) && self::isSlash($filename[1]);
 		}
 		
 		// checks if is stream wrapper
-		// important: does not check, whether the stream wrapper actually exists
-		// expects: UNNORMALIZED slashes (file:\\ is not recognized, for sure)
-		public static function isStreamWrapper($filename, &$addinf = 'no_addinf') {
-			if (preg_match('#^([-+.a-zA-Z0-9]+)://(.*)$#', $filename, $matches)) {
-				if ($addinf != 'no_addinf') {
-					$addinf = array($matches[1], $matches[2]);
-				}
-				return true;
-			}
-			return false;
+		// (does not check, whether the stream wrapper actually exists)
+		// filename: must NOT be slash-normalized
+		public static function isStreamWrapper($filename) {
+			return preg_match('#^([-+.a-zA-Z0-9]+)://#', $filename);
 		}
 		
-		// internal!
 		// checks if $char is self::$slash
-		// expects: normalized slash
-		protected static function isSlash($char) {
+		// char: must be slash-normalized
+		private static function isSlash($char) {
 			return $char == self::$slash;
 		}
 	}

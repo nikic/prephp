6dce375d1bcba87e76a829ab438c4dc019785cde
nikic <privat@ni-po.com>
2010-09-03 17:54:19 +0200

    make namespace test PHP compliant

--- test/namespaces.php
+++ test/namespaces.php
@@ -1,15 +1,21 @@
 <?php
-    namespace {                // {
+    namespace {
         class HalloWorld {}    // class HalloWorld {}
         
         namespace\strlen('');  // strlen('');
-    }                          // }
+        
+        spl_autoload_register(function ($class) {
+            echo $class;
+        });
+        
+        spl_autoload_call('Some__N__Strange__N__Class__N__Name');
+    }
     
-    namespace Foo\Bar\Higher { // {
+    namespace Foo\Bar\Higher {
         class Hallo {}         // class Foo__N__Bar__N__Higher__N__Hallo {}
-    }                          // }
+    }
     
-    namespace Foo\Bar {        // {
+    namespace Foo\Bar {
         use \HalloWorld;       // 
         
         class Hallo {}         // class Foo__N__Bar__N__Hallo {}
@@ -19,11 +25,5 @@
         new Higher\Hallo;      // new Foo__N__Bar__N__Higher__N__Hallo;
         
         strlen('');            // call_user_func(prephp_rt_checkFunction('Foo__N__Bar__N__strlen','strlen'),'');
-    }                          // }
-    
-    spl_autoload_register(function ($class) {
-        echo $class;
-    });
-    
-    spl_autoload_call('Some__N__Strange__N__Class__N__Name');
+    }
 ?>
